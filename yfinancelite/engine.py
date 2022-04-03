04aa2dba89474df545aa99d3fbce12c53a37d1ca
= <=>
2022-04-03 15:40:50 +0300

    PEP8 formatting

--- yfinancelite/engine.py
+++ yfinancelite/engine.py
@@ -7,9 +7,11 @@ import json
 
 QUERY_URL = "https://query2.finance.yahoo.com"
 SCRAPE_URL = "https://finance.yahoo.com/quote/"
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-def _download_historical_data(ticker, start = None, end = None, events = False):
+
+def _download_historical_data(ticker, start=None, end=None, events=False):
     """Downlads historical data optionally events as well.
 
     Args:
@@ -25,12 +27,12 @@ def _download_historical_data(ticker, start = None, end = None, events = False):
     start, end = _calc_time(start, end)
     params = {"period1": start, "period2": end}
     params['interval'] = '1d'
-    if events: 
+    if events:
         params["events"] = "div,splits"
-    
+
     url = "{}/v8/finance/chart/{}".format(QUERY_URL, ticker)
     response = requests.get(url=url, params=params, headers=HEADERS)
-    
+
     data = response.json()
     events = _parse_events(data['chart']['result'][0]) if events else None
     df = _parse_quotes(data["chart"]["result"][0])
@@ -39,6 +41,7 @@ def _download_historical_data(ticker, start = None, end = None, events = False):
         return df, events
     else:
         return df
+
 
 def _download_summary_store(ticker):
     """Download whole information about ticker.
@@ -52,11 +55,13 @@ def _download_summary_store(ticker):
     url = SCRAPE_URL + ticker
     response = requests.request('GET', url=url, headers=HEADERS).text
     json_str = response.split('root.App.main =')[1].split(
-            '(this)')[0].split(';\n}')[0].strip()
-    data = json.loads(json_str)['context']['dispatcher']['stores']['QuoteSummaryStore']
+        '(this)')[0].split(';\n}')[0].strip()
+    data = json.loads(json_str)[
+        'context']['dispatcher']['stores']['QuoteSummaryStore']
     return data
 
-def download_batch(tickers, start = None, end = None):
+
+def download_batch(tickers, start=None, end=None):
     batch = Parallel(n_jobs=-1, backend='threading')(
         delayed(_download_historical_data)(ticker, start=start, end=end, events=False) for ticker in tickers)
     df = {ticker.upper(): batch[i] for i, ticker in enumerate(tickers)}
